8192a8b38540ca5ffc27a91c3d62552169d827b0
hugo <martin.romero.hugo@iescamas.es>
2024-10-23 18:43:41 +0200

    Diseño app terminado 100%

--- app/src/main/java/com/fireboy/fichapersonal/MainActivity.java
+++ app/src/main/java/com/fireboy/fichapersonal/MainActivity.java
@@ -1,19 +1,33 @@
 package com.fireboy.fichapersonal;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnNombre, btnCurso, btnLenguajes, btnNuevoRegistro;
+    TextView lblNombre, lblCurso, lblLenguajes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initComponents();
+
+
+    }
+
+    private void initComponents() {
+        btnNombre = findViewById(R.id.btnNombre);
+        btnCurso = findViewById(R.id.btnCurso);
+        btnLenguajes = findViewById(R.id.btnLenguajes);
+        btnNuevoRegistro = findViewById(R.id.btnNuevoRegistro);
+
+        lblNombre = findViewById(R.id.lblNombre);
+        lblCurso = findViewById(R.id.lblCurso);
+        lblLenguajes = findViewById(R.id.lblLenguajes);
     }
 }
